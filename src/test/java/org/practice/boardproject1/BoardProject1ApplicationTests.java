5836d28464d1c3d512f100f7dc51ffdaf921a16e
junseong <tjd782323@gmail.com>
2024-12-25 20:14:39 +0900

    스프링부트 프로젝트 초기화

---
+++ src/test/java/org/practice/boardproject1/BoardProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practice.boardproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
