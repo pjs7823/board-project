daf7f43cb6ebcbf4dab2ff1f740bdc2902dc4667
junseong <tjd782323@gmail.com>
2024-12-29 17:40:55 +0900

    수정

--- src/main/java/org/practice/boardproject1/domain/ArticleComment.java
+++ src/main/java/org/practice/boardproject1/domain/ArticleComment.java
@@ -1,11 +1,27 @@
 package org.practice.boardproject1.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.ToString;
+
 import java.time.LocalDateTime;
 
+@Getter
+@ToString
+@Table(indexes= {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdBy"),
+        @Index(columnList = "createdBy")
+})
+@Entity
+
 public class ArticleComment {
-    private Long id;
-    private Article article;
-    private String content;
+    private Long id; // 댓글 ID
+    private Article article; // 게시글 ID
+    private String content; // 댓글 본문
 
     private LocalDateTime createdAt;
     private String createdBy;
